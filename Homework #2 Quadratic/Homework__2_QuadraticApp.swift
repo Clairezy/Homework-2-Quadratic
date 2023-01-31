6d9b118a5f2d839a8ce3f8be5de055ddbb5d7e1c
Clairezy <clairezwicker98@gmail.com>
2023-01-30 20:11:53 -0600

    Initial Commit

---
+++ Homework #2 Quadratic/Homework__2_QuadraticApp.swift
@@ -0,0 +1,17 @@
+//
+//  Homework__2_QuadraticApp.swift
+//  Homework #2 Quadratic
+//
+//  Created by Claire on 1/30/23.
+//
+
+import SwiftUI
+
+@main
+struct Homework__2_QuadraticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
